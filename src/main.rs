eec641cb1da4647955135a5d45574a4cd9b94267
Sönke Krusche <soenke.krusche@msg-david.de>
2021-10-06 17:32:51 +0200

    a

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{env, process};
-use std::fs;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,29 +13,13 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    run(config)
-}
-
-fn run(config: Config) {
-    let contents = fs::read_to_string(&config.filename)
-        .expect(format!("Something went wrong with reading file {}", config.filename).as_str());
-    println!("With text:\n{}", contents)
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        //TODO: use more efficient way to fix ownership issue than clone. Check chapter 13 rustbook
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
     }
 }
+
+
+
+
+
